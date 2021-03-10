bb38bb69af4e08908cd11ed81b18f26a90e5dd5a
christinanikolova <christina.nikolova@abv.bg>
2021-03-10 07:52:40 +0200

    work om DB's configurations

---
+++ server/RecipesWebApi/Data/Recipes.Data/Configurations/RecipeLikeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipes.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Recipes.Data.Models;
+
+    public class RecipeLikeConfiguration : IEntityTypeConfiguration<RecipeLike>
+    {
+        public void Configure(EntityTypeBuilder<RecipeLike> recipeLike)
+        {
+            recipeLike
+                .HasKey(rl => new { rl.RecipeId, rl.UserId });
+        }
+    }
+}
---
+++ server/RecipesWebApi/Data/Recipes.Data/Configurations/RepiceIngredientConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipes.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Recipes.Data.Models;
+
+    public class RepiceIngredientConfiguration : IEntityTypeConfiguration<RecipeIngredient>
+    {
+        public void Configure(EntityTypeBuilder<RecipeIngredient> repiceIngredient)
+        {
+            repiceIngredient
+                .HasKey(ri => new { ri.IngredientId, ri.RecipeId });
+        }
+    }
+}
