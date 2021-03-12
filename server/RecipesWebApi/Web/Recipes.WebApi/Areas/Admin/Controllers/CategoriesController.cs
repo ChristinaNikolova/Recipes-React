01cbfe35a43b87e3402c95f05f11f241f9578c66
christinanikolova <christina.nikolova@abv.bg>
2021-03-12 07:26:02 +0200

    add administration on the server

---
+++ server/RecipesWebApi/Web/Recipes.WebApi/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,180 @@
+﻿namespace Recipes.WebApi.Areas.Admin.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Common;
+    using Recipes.Services.Data.Categories;
+    using Recipes.Web.Models.Admin.Categories.InputModels;
+    using Recipes.Web.Models.Admin.Categories.ViewModels;
+    using Recipes.Web.Models.Common.ViewModels;
+    using Recipes.WebApi.Controllers;
+
+    [Route("api/admin/[controller]/[action]")]
+    public class CategoriesController : ApiController
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Create([FromBody] CategoryInputModel input)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                var isCategoryAlreadyExisting = await this.categoriesService.IsCategoryAlreadyExistingAsync(input.Name);
+
+                if (isCategoryAlreadyExisting)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.AlreadyExistsCategory,
+                    });
+                }
+
+                try
+                {
+                    await this.categoriesService.CreateAsync(input.Name, input.Picture);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Added,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<IEnumerable<CategoryAdminViewModel>>> All()
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    var categories = await this.categoriesService.GetAllAsync<CategoryAdminViewModel>();
+
+                    return new List<CategoryAdminViewModel>(categories);
+                }
+                catch (Exception ex)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = ex.Message,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<CategoryUpdateInputModel>> CategoryForUpdate(string id)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    var category = await this.categoriesService.GetDetailsAsync<CategoryUpdateInputModel>(id);
+
+                    return category;
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    await this.categoriesService.DeleteAsync(id);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Deleted,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update(CategoryUpdateInputModel input)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    await this.categoriesService.UpdateAsync(input.Id, input.Name, input.Picture);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Updated,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+    }
+}
---
+++ server/RecipesWebApi/Web/Recipes.WebApi/Areas/Admin/Controllers/IngredientsController.cs
@@ -0,0 +1,180 @@
+﻿namespace Recipes.WebApi.Areas.Admin.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Recipes.Common;
+    using Recipes.Services.Data.Ingredients;
+    using Recipes.Web.Models.Admin.Ingredients.InputModels;
+    using Recipes.Web.Models.Admin.Ingredients.ViewModels;
+    using Recipes.Web.Models.Common.ViewModels;
+    using Recipes.WebApi.Controllers;
+
+    [Route("api/admin/[controller]/[action]")]
+    public class IngredientsController : ApiController
+    {
+        private readonly IIngredientsService ingredientsService;
+
+        public IngredientsController(IIngredientsService ingredientsService)
+        {
+            this.ingredientsService = ingredientsService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Create([FromBody] IngredientCreateAdminInputModel input)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                var isIngredientAlreadyExisting = await this.ingredientsService.IsAlreadyAddedAsync(input.Name);
+
+                if (isIngredientAlreadyExisting)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.AlreadyExistsIngredient,
+                    });
+                }
+
+                try
+                {
+                    await this.ingredientsService.CreateAsync(input.Name);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Added,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<IEnumerable<IngredientAdminViewModel>>> All()
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    var ingredients = await this.ingredientsService.GetAllAsync<IngredientAdminViewModel>();
+
+                    return new List<IngredientAdminViewModel>(ingredients);
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<IngredientUpdateInputModel>> IngredientForUpdate(string id)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    var ingredient = await this.ingredientsService.GetDetailsAsync<IngredientUpdateInputModel>(id);
+
+                    return ingredient;
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    await this.ingredientsService.DeleteAsync(id);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Deleted,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update(IngredientUpdateInputModel input)
+        {
+            if (this.User.Identity.Name == GlobalConstants.Roles.Admin)
+            {
+                try
+                {
+                    await this.ingredientsService.UpdateAsync(input.Id, input.Name);
+
+                    return this.Ok(new
+                    {
+                        Message = Messages.Success.Updated,
+                    });
+                }
+                catch (Exception)
+                {
+                    return this.BadRequest(new BadRequestViewModel
+                    {
+                        Message = Messages.Error.Unknown,
+                    });
+                }
+            }
+
+            return this.Unauthorized();
+        }
+    }
+}
