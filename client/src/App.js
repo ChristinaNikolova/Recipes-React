4209b66514817a9529b6589c095950aff7ee75c5
christinanikolova <christina.nikolova@abv.bg>
2021-03-21 10:30:21 +0200

    add Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import {Route, Switch, Redirect} from 'react-router-dom';
+
 import Footer from './components/shared/Footer/Footer';
 import Home from './components/Home/Home';
 import Header from './components/shared/Header/Header';
@@ -8,7 +10,12 @@ function App() {
     return (
         <div className="app">
             <Header/>
-            <Home/>
+            <Switch>
+                <Route path='/' exact>
+                    <Redirect to='/home'></Redirect>
+                </Route>
+                <Route path='/home' component={Home}></Route>
+            </Switch>
             <Footer/>
         </div>
     );
