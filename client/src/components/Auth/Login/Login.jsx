80d536f25bb597673d0758b851383e5c1264d9ef
christinanikolova <christina.nikolova@abv.bg>
2021-03-28 12:51:55 +0300

    add CSS and HTML to login and register forms

--- client/src/components/Auth/Login/Login.jsx
+++ client/src/components/Auth/Login/Login.jsx
@@ -1,8 +1,32 @@
+import Input from '../../shared/Input/Input.jsx';
+
 import './Login.css';
 
 function Login() {
     return (
-        <h1>Login</h1>
+        <div className="login-wrapper">
+            <div className="fill pt-2 pb-2"></div>
+            <div className="container">
+                <h1 className="p-1 cursive-font-style">Sign In</h1>
+                <div className="row">
+                    <div className="col-lg-10">
+                        <form className="mt-2">
+                            <Input
+                                type='email'
+                                name='email'
+                                label='Email' />
+
+                            <Input
+                                type='password'
+                                name='password'
+                                label='Password' />
+
+                            <button type="submit" class="btn btn-dark">Sign In</button>
+                        </form>
+                    </div>
+                </div >
+            </div >
+        </div >
     );
 }
 
--- client/src/components/Auth/Register/Register.jsx
+++ client/src/components/Auth/Register/Register.jsx
@@ -1,8 +1,42 @@
+import Input from '../../shared/Input/Input.jsx';
+
 import './Register.css';
 
 function Register() {
     return (
-        <h1>Register...</h1>
+        <div className="register-wrapper">
+            <div className="fill pt-2 pb-2"></div>
+            <div className="container">
+                <h1 className="p-1 cursive-font-style">Register</h1>
+                <div className="row">
+                    <div className="col-lg-10">
+                        <form>
+                            <Input
+                                type='text'
+                                name='username'
+                                label='Username' />
+
+                            <Input
+                                type='email'
+                                name='email'
+                                label='Email' />
+
+                            <Input
+                                type='password'
+                                name='password'
+                                label='Password' />
+
+                            <Input
+                                type='password'
+                                name='repeatPassword'
+                                label='Repeat password' />
+
+                            <button className="btn btn-dark" type="submit" > Register</button >
+                        </form >
+                    </div >
+                </div >
+            </div >
+        </div >
     );
 }
 
