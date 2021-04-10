01dc4b0b615212b84093ee9433d47c23d6d188b1
christinanikolova <christina.nikolova@abv.bg>
2021-04-10 15:46:40 +0300

    add final touches

--- client/src/components/Recipe/RecipeDetails/RecipeDetails.jsx
+++ client/src/components/Recipe/RecipeDetails/RecipeDetails.jsx
@@ -11,7 +11,7 @@ import './RecipeDetails.css';
 class RecipeDetails extends Component {
     constructor(props) {
         super(props)
-
+        
         this.state = {
             recipe: {}
         };
