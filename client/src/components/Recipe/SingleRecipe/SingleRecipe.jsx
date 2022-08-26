20d1c3572f2a7cfb8eadbf3252eef708cfa9e83e
christinanikolova <christinanikolova@github.com>
2022-08-26 17:13:09 +0300

    fix some css issues

--- client/src/components/Recipe/SingleRecipe/SingleRecipe.jsx
+++ client/src/components/Recipe/SingleRecipe/SingleRecipe.jsx
@@ -5,31 +5,29 @@ import './SingleRecipe.css';
 
 function SingleRecipe({ id, title, picture, shortContent, categoryId, categoryName, recipeLikesCount, commentsCount }) {
     return (
-        <div className="col-lg-4">
-            <div className="card mb-2 custom-card-recipe">
-                <h3 className="card-header recipe-title cursive-font-style">{title}</h3>
-                <img className="img-recipe" src={picture} alt="recipe-pic"></img>
-                <div className="card-body">
-                    <p className="card-text">{shortContent}...</p>
-                </div>
-                <ul className="list-group list-group-flush recipe-details">
-                    <li className="list-group-item">
-                        <i className="fas fa-book"></i>
-                        Category:
-                        <Link to={`/recipes/current-category/${categoryId}`} className="hover-effect">{categoryName}</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <i className="fas fa-heart"></i>
-                        Likes: {recipeLikesCount}</li>
-                    <li className="list-group-item">
-                        <i className="fas fa-comments"></i>
-                        Comments: {commentsCount}</li>
-                </ul>
-                <div className="card-body">
-                    <Link to={`/recipes/details/${id}`}><button className="custom-btn">See the recipe</button></Link>
-                </div>
+        <article className="card custom-card-recipe">
+            <h3 className="card-header recipe-title cursive-font-style">{title}</h3>
+            <img className="img-recipe" src={picture} alt="recipe-pic"></img>
+            <div className="card-body">
+                <p className="card-text">{shortContent}...</p>
             </div>
-        </div>
+            <ul className="list-group list-group-flush recipe-details">
+                <li className="list-group-item">
+                    <i className="fas fa-book"></i>
+                    Category:
+                    <Link to={`/recipes/current-category/${categoryId}`} className="hover-effect">{categoryName}</Link>
+                </li>
+                <li className="list-group-item">
+                    <i className="fas fa-heart"></i>
+                    Likes: {recipeLikesCount}</li>
+                <li className="list-group-item">
+                    <i className="fas fa-comments"></i>
+                    Comments: {commentsCount}</li>
+            </ul>
+            <div className="card-body">
+                <Link to={`/recipes/details/${id}`}><button className="custom-btn">See the recipe</button></Link>
+            </div>
+        </article>
     );
 }
 
